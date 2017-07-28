d13bf0cb3f62102e56ce6a97498dab978ae485a2
sqz777 <sqz777isme@gmail.com>
2017-07-28 19:28:09 +0800

    initial

---
+++ Codewars_BuildPalindrome/Codewars_BuildPalindrome/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Codewars_BuildPalindrome")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Codewars_BuildPalindrome")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f884c1e8-f5bf-499e-80a0-a49bb99e51d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
