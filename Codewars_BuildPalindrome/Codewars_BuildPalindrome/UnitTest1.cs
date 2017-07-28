f320ffe205baf3d4b6fb38564e3d8495de45ec2d
sqz777 <sqz777isme@gmail.com>
2017-07-28 19:31:06 +0800

    add first test case and failed

--- Codewars_BuildPalindrome/Codewars_BuildPalindrome/UnitTest1.cs
+++ Codewars_BuildPalindrome/Codewars_BuildPalindrome/UnitTest1.cs
@@ -7,9 +7,21 @@ namespace Codewars_BuildPalindrome
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_abc_Should_Be_abcba()
         {
+            var kata = new Kata();
+            var input = "abc";
+            var actual = kata.BuildPalindrome(input);
+            var expected = "abcba";
+            Assert.AreEqual(expected,actual);
+        }
+    }
 
+    public class Kata
+    {
+        public string BuildPalindrome(string input)
+        {
+            throw new NotImplementedException();
         }
     }
 }
