72ac0011a773d64a99701d231bcefef542135536
TitanDarren <sqz777isme@gmail.com>
2017-07-31 14:32:26 +0800

    initial and add first test

---
+++ Codewars_OnesAndZeros/Codewars_OnesAndZeros/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Codewars_OnesAndZeros")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Titansoft")]
+[assembly: AssemblyProduct("Codewars_OnesAndZeros")]
+[assembly: AssemblyCopyright("Copyright © Titansoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd7d8667-f1f9-49ed-b24f-59edd6d3cbee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Codewars_OnesAndZeros/Codewars_OnesAndZeros/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_OnesAndZeros
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Input_0_Should_Be_0()
+        {
+            var kata = new Kata();
+            var input = new[] {0};
+            var actual = kata.BinaryArrayToNumber(input);
+            var expected = 0;
+            Assert.AreEqual(expected,actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int BinaryArrayToNumber(int[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
